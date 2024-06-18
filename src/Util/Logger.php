3491703c0e65a4559616e861b7a9f0d3836d6fd9
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2024-06-18 14:39:10 +0200

    Add simple logging

--- src/Util/Logger.php
+++ src/Util/Logger.php
@@ -11,14 +11,61 @@ class Logger {
 	const INFO    = 'info';
 
 	/**
-	 * Logging wrapper. Fires the `newspack_migration_tools_log` action.
+	 * Simple log to file.
 	 *
-	 * @param string $filename File name or path.
-	 * @param string $message Log message.
-	 * @param string $level Log level. See constants in this class.
+	 * If you want fancier logging, use the `newspack_migration_tools_log` action. You can disable the simple
+	 * logging by using the `newspack_migration_tools_log_disable_default` filter.
+	 *
+	 * @param string $file          File name or path.
+	 * @param string $message       Log message.
+	 * @param string $level         Log level. See constants in this class.
 	 * @param bool   $exit_on_error Whether to exit on error.
 	 */
-	public static function log( string $filename, string $message, string $level = self::LINE, bool $exit_on_error = false ) {
-		do_action( 'newspack_migration_tools_log', $filename, $message, $level, $exit_on_error );
+	public static function log( $file, $message, $level = 'line', bool $exit_on_error = false ) {
+		/**
+		 * Filter the file path for the log file.
+		 *
+		 * @since 0.0.1
+		 *
+		 * @param string $file The file path.
+		 */
+		$file_path = apply_filters( 'newspack_migration_tools_log_file_path_default', $file );
+
+		/**
+		 * Fires before default logging.
+		 *
+		 * @since 0.0.1
+		 *
+		 * @param string $file          File name or path.
+		 * @param string $message       Log message.
+		 * @param string $level         Log level. See constants in this class.
+		 * @param bool   $exit_on_error Whether to exit on error.
+		 */
+		do_action( 'newspack_migration_tools_log', $file, $message, $level, $exit_on_error );
+
+		/**
+		 * Filter to disable default logging.
+		 *
+		 * @since 0.0.1
+		 *
+		 * @param bool $disable_default If not false then disable default logging.
+		 */
+		if ( apply_filters( 'newspack_migration_tools_log_disable_default', false ) ) {
+			return;
+		}
+
+		// Allow disabling default logging.
+		if ( defined( 'NEWSPACK_MIGRATION_TOOLS_DISABLE_DEFAULT_LOGGING' ) && ! empty( NEWSPACK_MIGRATION_TOOLS_DISABLE_DEFAULT_LOGGING ) ) {
+			return;
+		}
+
+		if ( in_array( $level, [ self::SUCCESS, self::WARNING, self::ERROR ], true ) ) {
+			// Prepend the level to the message for easier grepping in the log file.
+			$message = sprintf( '%s: %s', strtoupper( $level ), $message );
+		}
+
+		file_put_contents( $file_path, "{$message}\n", FILE_APPEND );
+		echo "{$message}\n";
 	}
+
 }
