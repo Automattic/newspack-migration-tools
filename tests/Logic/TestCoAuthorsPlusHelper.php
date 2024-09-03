14603cba20e98e7d4843d60414b8148fcb3c9a4c
ronchambers <ronchambers@gmail.com>
2024-09-03 10:41:53 -0700

    Test: CoAuthorsPlus: assign author to post

--- tests/Logic/TestCoAuthorsPlusHelper.php
+++ tests/Logic/TestCoAuthorsPlusHelper.php
@@ -51,4 +51,25 @@ class TestCoAuthorsPlusHelper extends WP_UnitTestCase {
 			]
 		);
 	}
+
+	/**
+	 * Test assign authors to post.
+	 */
+	public function test_assign_authors_to_post() {
+
+		// Create a post.
+		$post_id = self::factory()->post->create();
+
+		// Create a GA.
+		$helper = new CoAuthorsPlusHelper();
+		$ga_id = $helper->create_guest_author( array( 'display_name' => 'Test User' ) );
+		$ga = $helper->get_guest_author_by_id( $ga_id );
+
+		// Try to assign author to post.
+		// assign_authors_to_post will throw exception on failure.
+		// if no exception, assert success.
+		$helper->assign_authors_to_post( array( $ga ), $post_id );
+		$this->assertTrue( true );
+
+	}
 }
