95395fd58992a0e0c19a1e25c2cbb51c2be883e1
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2024-09-04 09:44:31 +0200

    Use closure

--- src/Command/WpCliCommandTrait.php
+++ src/Command/WpCliCommandTrait.php
@@ -2,10 +2,11 @@
 
 namespace Newspack\MigrationTools\Command;
 
+use Closure;
+use Newspack\MigrationTools\Log\CliLogger;
+
 /**
- * Utility trait for implementing the WpCliCommandInterface interface.
- *
- * Use this trait so you don't have to copy-paste the constructor.
+ * Utility trait for ensuring singleton instances of WP CLI commands.
  */
 trait WpCliCommandTrait {
 
@@ -16,5 +17,42 @@ trait WpCliCommandTrait {
 	 */
 	private function __construct() {
 		// Nothing.
+	}
+
+	/**
+	 * Singleton instance getter.
+	 *
+	 * @return self
+	 */
+	public static function get_instance(): self {
+		static $instance = null;
+
+		if ( null === $instance ) {
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Get a closure wrapping the command function.
+	 *
+	 * We do this to make sure we instantiate the command class as late as possible.
+	 *
+	 * @param string $command_name The function name of the command to run when the command is invoked.
+	 *
+	 * @return Closure
+	 */
+	protected static function get_command_closure( string $command_name ): Closure {
+		if ( ! method_exists( self::get_instance(), $command_name ) ) {
+			CliLogger::error(
+				sprintf( 'Command "%s" does not exist in %s', $command_name, get_class( self::get_instance() ) ),
+				true
+			);
+		}
+
+		return function ( array $pos_args, array $assoc_args ) use ( $command_name ) {
+			return self::get_instance()->{$command_name}( $pos_args, $assoc_args );
+		};
 	}
 }
