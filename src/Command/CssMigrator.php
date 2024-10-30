0fc7650a375bca0615a9c5d3092c70269d1b7bae
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2024-10-30 14:57:57 +0100

    CssMigrator

--- src/Command/CssMigrator.php
+++ src/Command/CssMigrator.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace NewspackCustomContentMigrator\Command\General;
+namespace Newspack\MigrationTools\Command;
 
-use Newspack\MigrationTools\Command\PostsMigrator;
-use Newspack\MigrationTools\Command\WpCliCommandTrait;
-use NewspackCustomContentMigrator\Command\RegisterCommandInterface;
 use WP_CLI;
 
-class CssMigrator implements RegisterCommandInterface {
+class CssMigrator implements WpCliCommandInterface {
 
 	use WpCliCommandTrait;
 
@@ -19,48 +16,49 @@ class CssMigrator implements RegisterCommandInterface {
 	/**
 	 * {@inheritDoc}
 	 */
-	public static function register_commands(): void {
-		WP_CLI::add_command(
-			'newspack-content-migrator export-current-theme-custom-css',
-			self::get_command_closure( 'cmd_export_current_theme_custom_css' ),
+	public static function get_cli_commands(): array {
+		return [
 			[
-			'shortdesc' => 'Exports custom CSS for current active Theme. Exits with code 0 on success or 1 otherwise.',
-			'synopsis'  => [
+				'newspack-content-migrator export-current-theme-custom-css',
+				self::get_command_closure( 'cmd_export_current_theme_custom_css' ),
 				[
-					'type'        => 'assoc',
-					'name'        => 'output-dir',
-					'description' => 'Output directory full path (no ending slash).',
-					'optional'    => false,
-					'repeating'   => false,
+					'shortdesc' => 'Exports custom CSS for current active Theme. Exits with code 0 on success or 1 otherwise.',
+					'synopsis'  => [
+						[
+							'type'        => 'assoc',
+							'name'        => 'output-dir',
+							'description' => 'Output directory full path (no ending slash).',
+							'optional'    => false,
+							'repeating'   => false,
+						],
+					],
 				],
 			],
-		] );
 
-		WP_CLI::add_command(
-			'newspack-content-migrator import-custom-css-file',
-			self::get_command_closure( 'cmd_import_custom_css_file' ),
 			[
-			'shortdesc' => 'Imports custom CSS which was exported from the Staging site.',
-			'synopsis'  => [
+				'newspack-content-migrator import-custom-css-file',
+				self::get_command_closure( 'cmd_import_custom_css_file' ),
 				[
-					'type'        => 'assoc',
-					'name'        => 'input-dir',
-					'description' => 'Input directory full path (no ending slash).',
-					'optional'    => false,
-					'repeating'   => false,
+					'shortdesc' => 'Imports custom CSS which was exported from the Staging site.',
+					'synopsis'  => [
+						[
+							'type'        => 'assoc',
+							'name'        => 'input-dir',
+							'description' => 'Input directory full path (no ending slash).',
+							'optional'    => false,
+							'repeating'   => false,
+						],
+					],
 				],
 			],
-		] );
+		];
 	}
 
 	/**
 	 * Callable for export-css command. Exits with code 0 on success or 1 otherwise.
-	 *
-	 * @param $args
-	 * @param $assoc_args
 	 */
 	public function cmd_export_current_theme_custom_css( $args, $assoc_args ) {
-		$output_dir = isset( $assoc_args[ 'output-dir' ] ) ? $assoc_args[ 'output-dir' ] : null;
+		$output_dir = isset( $assoc_args['output-dir'] ) ? $assoc_args['output-dir'] : null;
 		if ( is_null( $output_dir ) || ! is_dir( $output_dir ) ) {
 			WP_CLI::error( 'Invalid output dir.' );
 		}
@@ -70,41 +68,38 @@ class CssMigrator implements RegisterCommandInterface {
 		$result = $this->export_current_theme_custom_css( $output_dir, self::CSS_CURRENT_THEME_EXPORT_FILE );
 		if ( true === $result ) {
 			WP_CLI::success( 'Done.' );
-			exit(0);
+			exit( 0 );
 		} else {
 			WP_CLI::warning( 'Done with warnings.' );
-			exit(1);
+			exit( 1 );
 		}
 	}
 
 	/**
 	 * Exports current active Theme's custom CSS to a file.
 	 *
-	 * @param $output_dir
-	 * @param $file_output_css
+	 * @param string $output_dir Directory.
+	 * @param string $file_output_css File name to output the CSS.
 	 *
 	 * @return bool Success.
 	 */
 	public function export_current_theme_custom_css( $output_dir, $file_output_css ) {
 		wp_cache_flush();
 		$theme_mods = get_theme_mods();
-		if ( ! isset( $theme_mods[ 'custom_css_post_id' ] ) || empty( $theme_mods[ 'custom_css_post_id' ] ) ) {
+		if ( ! isset( $theme_mods['custom_css_post_id'] ) || empty( $theme_mods['custom_css_post_id'] ) ) {
 			return false;
 		}
 
-		$post_id = $theme_mods[ 'custom_css_post_id' ];
+		$post_id = $theme_mods['custom_css_post_id'];
 
 		return PostsMigrator::get_instance()->migrator_export_posts( array( $post_id ), $output_dir, $file_output_css );
 	}
 
 	/**
 	 * Callable for import-custom-css-file command.
-	 *
-	 * @param $args
-	 * @param $assoc_args
 	 */
 	public function cmd_import_custom_css_file( $args, $assoc_args ) {
-		$input_dir = isset( $assoc_args[ 'input-dir' ] ) ? $assoc_args[ 'input-dir' ] : null;
+		$input_dir = isset( $assoc_args['input-dir'] ) ? $assoc_args['input-dir'] : null;
 		if ( is_null( $input_dir ) || ! is_dir( $input_dir ) ) {
 			WP_CLI::error( 'Invalid input dir.' );
 		}
@@ -112,7 +107,7 @@ class CssMigrator implements RegisterCommandInterface {
 		$import_file = $input_dir . '/' . self::CSS_CURRENT_THEME_EXPORT_FILE;
 		if ( ! is_file( $import_file ) ) {
 			WP_CLI::warning( sprintf( 'CSS file not found %s.', $import_file ) );
-			exit(1);
+			exit( 1 );
 		}
 
 		WP_CLI::line( 'Importing custom CSS from ' . $import_file . ' ...' );
@@ -140,7 +135,7 @@ class CssMigrator implements RegisterCommandInterface {
 	private function delete_all_custom_css() {
 		wp_cache_flush();
 
-		$args = array(
+		$args  = array(
 			'posts_per_page' => -1,
 			'post_type'      => 'custom_css',
 		);
