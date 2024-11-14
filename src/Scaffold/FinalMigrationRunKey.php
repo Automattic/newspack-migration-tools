2fda9654e55696d58d2685958c9ce90737379cc9
Eddie Carrasco <eddies.code.shop@gmail.com>
2024-11-14 12:17:34 -0500

    feat: saving FinalMigrationRunKey concrete class.
    This could just be a concrete class, and we could get rid of the interface. For now, I've left it like this to give us a bit of flexibility while we still put things together.

--- src/Scaffold/FinalMigrationRunKey.php
+++ src/Scaffold/FinalMigrationRunKey.php
@@ -2,18 +2,90 @@
 
 namespace Newspack\MigrationTools\Scaffold;
 
+use Newspack\MigrationTools\Scaffold\Enum\MigrationStatus;
+use Newspack\MigrationTools\Scaffold\Contracts\Migration;
+use Newspack\MigrationTools\Scaffold\Contracts\MigrationRunKey;
+
+/**
+ * Represents a final migration run key.
+ */
 final class FinalMigrationRunKey implements MigrationRunKey {
 
-	private $run_key;
+	/**
+	 * The ID of the migration being executed.
+	 *
+	 * @var int The ID of the migration being executed.
+	 */
+	private int $migration_id;
 
-	public function __construct( string $run_key ) {
-		$this->run_key = $run_key;
+	/**
+	 * The version of the migration being executed.
+	 *
+	 * @var int The version of the migration being executed.
+	 */
+	private int $migration_version;
+
+	/**
+	 * The current migration status.
+	 *
+	 * @var MigrationStatus $status The current migration status.
+	 */
+	private MigrationStatus $status;
+
+	/**
+	 * The migration being executed.
+	 *
+	 * @var Migration $migration The migration being executed.
+	 */
+	private Migration $migration;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param int       $migration_id The ID of the migration being executed.
+	 * @param int       $migration_version The version of the migration being executed.
+	 * @param Migration $migration The migration being executed.
+	 */
+	public function __construct( int $migration_id, int $migration_version, Migration $migration ) {
+		$this->migration_id      = $migration_id;
+		$this->migration_version = $migration_version;
+		$this->migration         = $migration;
+	}
+
+
+	/**
+	 * Returns the ID of the migration run key.
+	 *
+	 * @return int The ID of the migration run key.
+	 */
+	public function get_migration_id(): int {
+		return $this->migration_id;
 	}
 
 	/**
-	 * @inheritDoc
+	 * Returns the version of the migration run key.
+	 *
+	 * @return int
 	 */
-	public function get(): string {
-		return $this->run_key;
+	public function get_migration_version(): int {
+		return $this->migration_version;
+	}
+
+	/**
+	 * Returns the enum status of the migration run key.
+	 *
+	 * @return MigrationStatus
+	 */
+	public function get_migration_status(): MigrationStatus {
+		return $this->status;
+	}
+
+	/**
+	 * The migration object.
+	 *
+	 * @return Migration The migration object.
+	 */
+	public function get_migration(): Migration {
+		return $this->migration;
 	}
 }
