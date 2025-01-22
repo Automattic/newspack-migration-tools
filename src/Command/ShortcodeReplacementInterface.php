23ff9b32f66134fead07120f89e541f16905d301
Ivan Uravic <iuravic@gmail.com>
2025-01-22 12:34:07 +0100

    Improve interface

--- src/Command/ShortcodeReplacementInterface.php
+++ src/Command/ShortcodeReplacementInterface.php
@@ -4,18 +4,18 @@ namespace Newspack\MigrationTools\Command;
 
 /**
  * Used by the ShortcodesMigrator, `replace-shortcodes-in-post-body` command's `replace-callback` argument.
- * Create a class method by implementing this interface which will take a shortcode name and a post_id where
- * the shortcode is located, and return a replacement for that shortcode.
+ * Create a class method by implementing this interface which will take a shortcode string and the post ID where
+ * this shortcode is located, and return a replacement for the shortcode.
  */
 interface ShortcodeReplacementInterface {
 	/**
-	 * Callback which generates a replacement for a shortcode.
+	 * Return a replacement for a shortcode.
 	 *
-	 * @param string $shortcode_name Shortcode name.
-	 * @param int    $post_id        Post ID where the shortcode is being replaced. Use this if needed for more context,
-	 *                               e.g. if you wish to get the post author, title, or anything that might be required
-	 *                               to generate the replacement.
-	 * @return string HTML replacement for the shortcode.
+	 * @param string $shortcode Shortcode string.
+	 * @param int    $post_id   Post ID where the shortcode is being replaced. Use for more context if needed,
+	 *                          e.g. to get the post author, title, or anything that might be required
+	 *                          to generate the replacement.
+	 * @return string|false Any HTML replacement for the shortcode. False if no adequate replacement is generated.
 	 */
-	public function replace_shortcode( string $shortcode_name, int $post_id ): string;
+	public function replace_shortcode( string $shortcode, int $post_id ): string|false;
 }
