4db9a5ff897b8031e1211e5538b4571d61bd2157
Ivan Uravic <iuravic@gmail.com>
2025-08-05 17:56:40 +0200

    Add comments

--- src/Logic/Bylines.php
+++ src/Logic/Bylines.php
@@ -14,19 +14,30 @@ class Bylines {
 	 * - filters out unsupported characters,
 	 * - applies manual substitutions,
 	 *     e.g. when you wish to preserve certain unexploded bylines. Let's say you have a separator ',',
-	 *     but you wish to preserve 'Arthur Author, Ph.D.' as a single author name, or even remove the 'Ph.D.' suffix.   * 
+	 *     but you wish to preserve 'Arthur Author, Ph.D.' as a single author name, or even remove the
+	 *     'Ph.D.' suffix.   * 
 	 *
 	 * @param string $byline          Byline with one or multiple authors.
-	 * @param array  $separators      Separators to explode. Typical separators could be: [ '&', ', and ', ',', ' and ', ' y ' ].
+	 * @param array  $separators      Separators to explode. Typical separators could be:
+	 *                                  [ '&', ', and ', ',', ' and ', ' y ' ].
 	 *                                ⚠️ Important:
-	 *                                  - the order of separators matters. One separator is a substring of another separator (e.g. ',' and ', and'), make sure to explode by the longer separator first to avoid incorrect splitting.
-	 *                                  - be mindful of spaces used to surround separators (e.g. ' and ' vs 'and').
+	 *                                  - the order of separators matters. One separator is a substring of
+	 *                                    another separator (e.g. ',' and ', and'), make sure to explode by
+	 *                                    the longer separator first to avoid incorrect splitting.
+	 *                                  - be mindful of spaces used to surround separators (e.g. ' and '
+	 *                                    vs 'and').
 	 * @param array  $manual_bylines  Keys are bylines and values are resulting author names.
-	 *                                If there are some bylines that require special handling, you can specify the entire byline as a key, and the final author names as subarray with one or more values.
-	 *                                This will skip parsing these bylines and just return them, e.g. [ 'Arthur Author, Ph.D.' => [ 'Arthur Author' ] ].
-	 * @param array  $remove_prefixes Prefixes to remove from beginning of byline's author names, case-insensitive. E.g. 'By ' or 'Byline: '.
-	 * @param array  $remove_suffixes Suffixes to remove from end of byline's author names, case-insensitive. E.g. ', Daily News', or '| Daily News', etc..
-	 * @param array  $remove_chars    Characters to remove. Unsupported polluting characters found in byline metas.
+	 *                                If there are some bylines that require special handling, you can
+	 *                                specify the entire byline as a key, and the final author names as
+	 *                                subarray with one or more values.
+	 *                                This will skip parsing these bylines and just return them,
+	 *                                e.g. [ 'Arthur Author, Ph.D.' => [ 'Arthur Author' ] ].
+	 * @param array  $remove_prefixes Prefixes to remove from beginning of byline's author names,
+	 *                                case-insensitive. E.g. 'By ' or 'Byline: '.
+	 * @param array  $remove_suffixes Suffixes to remove from end of byline's author names,
+	 *                                case-insensitive. E.g. ', Daily News', or '| Daily News', etc.
+	 * @param array  $remove_chars    Characters to remove. Unsupported polluting characters found in
+	 *                                byline metas.
 	 * 
 	 * @return string[]               Exploded author names from byline.
 	 */
