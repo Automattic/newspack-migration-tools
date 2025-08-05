ea965eaa22dd9ceac3343c1fd89e7d28071d8f45
Ivan Uravic <iuravic@gmail.com>
2025-08-05 13:56:22 +0200

    Test manual substitutions

--- tests/Logic/BylinesTest.php
+++ tests/Logic/BylinesTest.php
@@ -1,20 +1,19 @@
 <?php
 /**
- * Tests for the Posts class.
+ * Tests for the Bylines class.
  */
 
 namespace Newspack\MigrationTools\Tests\Logic;
 
 use Newspack\MigrationTools\Logic\Bylines;
 use WP_UnitTestCase;
-use WP_Error;
 
 /**
- * Test the Posts class.
+ * Test the Bylines class.
  */
 class BylinesTest extends WP_UnitTestCase {
 	/**
-	 * The Posts instance.
+	 * The Bylines instance.
 	 *
 	 * @var Bylines
 	 */
@@ -29,23 +28,92 @@ class BylinesTest extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test parsing byline with single separator.
+	 * Test parse_byline() without separators.
 	 * 
-	 * @dataProvider data_explode_by_single_separator
+	 * @dataProvider data_byline_no_separators
 	 */
-	public function test_byline_explode_by_single_separator( string $byline, array $expected ) {
-		$result = $this->bylines->get_author_names_from_byline( $byline, [ ',' ] );
-		$this->assertEquals( $expected, $result );	
+	public function test_byline_no_separators( string $byline, array $separators, array $expected ) {
+		$result = $this->bylines->parse_byline( $byline, $separators );
+		$this->assertEquals( $expected, $result );
+	}
+
+	/**
+	 * Test parse_byline() using a single separator.
+	 * 
+	 * @dataProvider data_byline_explode_by_single_separator
+	 */
+	public function test_byline_explode_by_single_separator( string $byline, array $separators, array $expected ) {
+		$result = $this->bylines->parse_byline( $byline, $separators );
+		$this->assertEquals( $expected, $result );
+	}
+
+	/**
+	 * Test parse_byline() using manual substitutions.
+	 * 
+	 * @dataProvider data_byline_manual_substitutions
+	 */
+	public function test_byline_manual_substitutions( string $byline, array $separators, array $manual_substitutions, array $expected ) {
+		$result = $this->bylines->parse_byline( $byline, $separators, $manual_substitutions );
+		$this->assertEquals( $expected, $result );
 	}
 
 	/**
 	 * Data provider for test_byline_explode_by_single_separator.
 	 */
-	public function data_explode_by_single_separator() {
+	public function data_byline_no_separators() {
 		return [
-			[ 'John Doe', [ 'John Doe' ] ],
-			[ 'John Doe, Jane Doe', [ 'John Doe', 'Jane Doe' ] ],
-			[ 'John Doe, Jane Doe, Jim Doe', [ 'John Doe', 'Jane Doe', 'Jim Doe' ] ],
+			// No separators provided, nothing changes.
+			[ 'John Doe', [], [ 'John Doe' ] ],
+			[ 'John Doe, Jane Doe', [], [ 'John Doe, Jane Doe' ] ],
+		];
+	}
+
+	/**
+	 * Data provider for test_byline_explode_by_single_separator.
+	 */
+	public function data_byline_explode_by_single_separator() {
+		return [
+			// Single separator provided but not used.
+			[ 'John Doe', [ ',' ], [ 'John Doe' ] ],
+			[ 'John Doe', [ ' and ' ], [ 'John Doe' ] ],
+			[ 'John Doe', [ '&' ], [ 'John Doe' ] ],
+			// Different single separators.
+			[ 'John Doe, Jane Doe', [ ',' ], [ 'John Doe', 'Jane Doe' ] ],
+			[ 'John Doe and Jane Doe', [ ' and ' ], [ 'John Doe', 'Jane Doe' ] ],
+			[ 'John Doe & Jane Doe', [ '&' ], [ 'John Doe', 'Jane Doe' ] ],
+			// Wrong separator doesn't explode.
+			[ 'John Doe & Jane Doe', [ ' and ' ], [ 'John Doe & Jane Doe' ] ],
+		];
+	}
+
+	/**
+	 * Data provider for test_byline_manual_substitutions.
+	 */
+	public function data_byline_manual_substitutions() {
+		return [
+			[
+				'School of Journalism and Mass Communication',
+				[ ' and ' ],
+				[
+					'School of Journalism and Mass Communication' => [
+						'School of Journalism and Mass Communication',
+					],
+				],
+				[ 'School of Journalism and Mass Communication' ],
+			],
+			// Manual substitution is applied before exploding, this prevents returning [ 'John Doe', 'School of Journalism', 'Mass Communication' ] which would be wrong.
+			[
+				'John Doe and School of Journalism and Mass Communication',
+				[ ' and ' ],
+				[
+					'John Doe and School of Journalism and Mass Communication' => [
+						'John Doe',
+						'School of Journalism and Mass Communication',
+					],
+				],
+				[ 'John Doe', 'School of Journalism and Mass Communication' ],
+			],
+			
 		];
 	}
 }
