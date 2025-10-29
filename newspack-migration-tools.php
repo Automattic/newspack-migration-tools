83e8e8202cf665030dd2f1017ed869f0574dbbe1
Camilla Krag Jensen <camilla.krag.jensen@automattic.com>
2025-10-29 13:12:54 +0100

    Bump version to 1.0.2

--- newspack-migration-tools.php
+++ newspack-migration-tools.php
@@ -4,7 +4,7 @@
  *
  * This is a library to be included by plugins. See README.md for more information.
  *
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 8.3
  *
  * @package newspack-migration-tools
