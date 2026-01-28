d70b63bb015afa67b84f422af0a1a9fd420682ce
Ivan Uravic <iuravic@gmail.com>
2026-01-28 17:13:11 +0100

    Proper escaping for fgetcsv and fputcsv fix deprecation warnings

--- src/Util/CsvWriter.php
+++ src/Util/CsvWriter.php
@@ -58,8 +58,8 @@ class CsvWriter {
 	 * @throws Exception If the row cannot be written to the file.
 	 */
 	public function put( array $row ): void {
-		// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_fputcsv
-		if ( false === fputcsv( $this->file_pointer, $row ) ) {
+		// fputcsv escape='' for RFC 4180 compliance (@see https://www.php.net/manual/en/function.fputcsv.php).
+		if ( false === fputcsv( $this->file_pointer, $row, ',', '"', '' ) ) { // phpcs:ignore -- WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_fputcsv.
 			// phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
 			throw new Exception( "Could not write to file: {$this->filename}" );
 		}
