56fa29616c6d74ca5dbe3b88560785377d5f5945
ronchambers <116242607+ronchambers@users.noreply.github.com>
2026-07-16 12:02:21 -0700

    Fix Logging Should Adhere to Default Filters: logic set

--- src/Logic/GhostCMSHelper.php
+++ src/Logic/GhostCMSHelper.php
@@ -455,30 +455,18 @@ class GhostCMSHelper {
 		// Setup a logger. Be sure to use NMT's FileLog Util so that this log file will adhere to NMT's 
 		// logging filters such as `newspack_migration_tools_enable_file_log` and `newspack_migration_tools_log_dir`.
 		$output_file   = 'ghost_kg_elements.jsonl';
-		$output_file   = '/dev/full';
+		// $output_file   = '/dev/full';
 		// $output_file   = '/dev/null';
-
-		// $prefix = '';
-        // if ('file://' === substr($streamUrl, 0, 7)) {
-        //     $streamUrl = substr($streamUrl, 7);
-        //     $prefix = 'file://';
-		// /dev/null
-		//  See clearstatcache() for more details.
+		// $output_file = 'file:///dev/full';
 
 		$output_logger = FileLog::get_logger( $output_file, $output_file, new PlainLineFormatter() );
 
-		for( $i=0;$i<2;$i++) {
-			try {
-				FileLog::truncate_files( $output_logger );
-			} catch ( \Throwable $e ) {                   
-				$this->log( sprintf( "Failed to truncate log(s). %s", $e->getMessage() ), LogLevel::ERROR );
-				return;
-			}
-			$output_logger->info( 'asdfasdf' );
+		try {
+			FileLog::truncate_files( $output_logger );
+		} catch ( \Throwable $e ) {                   
+			$this->log( sprintf( "Failed to truncate log file(s). %s", $e->getMessage() ), LogLevel::ERROR );
+			return;
 		}
-
-
-
 
 		foreach ( $elements as $element_data ) {
 			$data = [
